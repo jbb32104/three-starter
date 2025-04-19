9faaa6f8093123cf0f9272d20306dbd8dec6538a
Jackson Barlow <jbb32104@uga.edu>
2025-04-19 13:20:25 -0400

    Next environment set up

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Next.js!</h1>
+      <p>
+        Get started by editing&nbsp;
+        <code>app/page.tsx</code>
+      </p>
+      <p>Get started by editing app/page.tsx</p>
+    </main>
+  );
+}
